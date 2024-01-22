bb43cc481d2462436f1d07344f00651919b1cdb2
Gökhan Bıyıkoğlu <gokhan_byk@icloud.com>
2024-01-22 16:16:09 +0300

    scroll animation with js

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-// Script
+const boxes = document.querySelectorAll('.box')
+
+window.addEventListener('scroll', checkBoxes)
+
+
+function checkBoxes() {
+    const triggerBottom = window.innerHeight / 5 * 4
+    
+    boxes.forEach(box => {
+        // A DOMReact describes the size and position of a rectangle
+        const boxTop = box.getBoundingClientRect().top
+
+        if (boxTop < triggerBottom) {
+            box.classList.add('show')
+        } else {
+            box.classList.remove('show')
+        }
+    })
+}
